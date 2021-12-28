5328c85dede6fd614f66e3b47854280f4cbf85f4
Dominic Cronin <dominic@cronin.tech>
2021-12-28 18:35:52 +0100

    Added global.json and GlobalUsing.cs

---
+++ src/GlobalUsings.cs
@@ -0,0 +1,26 @@
+global using System.Runtime.CompilerServices;
+
+global using Miniblog.Core;
+global using Miniblog.Core.Services;
+
+global using IWmmLogger = WebMarkupMin.Core.Loggers.ILogger;
+global using MetaWeblogService = Miniblog.Core.Services.MetaWeblogService;
+global using WmmNullLogger = WebMarkupMin.Core.Loggers.NullLogger;
+
+global using WilderMinds.MetaWeblog;
+
+global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.Linq;
+global using global::System.Net.Http;
+global using global::System.Net.Http.Json;
+global using global::System.Threading.Tasks;
+global using global::Microsoft.AspNetCore.Builder;
+global using global::Microsoft.AspNetCore.Hosting;
+global using global::Microsoft.AspNetCore.Http;
+global using global::Microsoft.AspNetCore.Mvc;
+global using global::Microsoft.AspNetCore.Routing;
+global using global::Microsoft.Extensions.Configuration;
+global using global::Microsoft.Extensions.DependencyInjection;
+global using global::Microsoft.Extensions.Hosting;
+global using global::Microsoft.Extensions.Logging;
